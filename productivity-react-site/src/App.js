726a10cf6857c5a77c7870eff337baff017909ed
Benjamin Pham <97713069+BenPham14@users.noreply.github.com>
2023-07-23 23:22:46 -0700

    Add todo list

--- productivity-react-site/src/App.js
+++ productivity-react-site/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
+import Tasks from './pages/tasks';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/tasks' element={<Tasks/>}/>
       </Routes>
     </HashRouter>
   );
---
+++ productivity-react-site/src/pages/tasks/index.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import Sidebar from "../../components/sidebar";
+import Topbar from "../../components/topbar";
+import tasks from "./Tasks.module.css";
+
+const TaskItem = ({id, label, deleteTask}) => {
+    return (
+        <>
+            <div className="flex">
+                <input type="checkbox" id={id} onClick={() => deleteTask(id)}></input>
+                <label htmlFor={id}>{label}</label>
+            </div>
+        </>
+    );
+}
+
+const Tasks = () => {
+    const [newItem, setNewItem] = useState("");
+    const [tasksList, setTaskList] = useState([]);
+
+    const addTask = (text) => {
+        if (!text) {
+            return
+        };
+
+        let item = {
+            id: Math.floor(Math.random() * 1000),
+            label: text
+        };
+        setTaskList([...tasksList, item]);
+        setNewItem("");
+    }
+
+    const deleteTask = (item) => {
+        let list = tasksList.filter((task) => task.id != item);
+        setTimeout(() => {
+            setTaskList(list);
+        }, 200);
+    }
+
+    const handleEnter = (event) => {
+        if(event.key === 'Enter') { 
+            addTask(newItem)
+        };
+    }
+
+    return (
+        <>
+            <Sidebar />
+            <main className={tasks.tasks}>
+                <Topbar />
+                <section className={`${tasks.list} flex column blk-shadow`}>
+                    <input 
+                        type="text" 
+                        value={newItem} 
+                        placeholder="+ Press Enter to add task..." 
+                        onChange={(event) => setNewItem(event.target.value)}
+                        onKeyDown={handleEnter}
+                    />
+                    {
+                        tasksList.map((task) => (
+                            <TaskItem 
+                                key={task.id}
+                                id={task.id}
+                                label={task.label}
+                                deleteTask={deleteTask}
+                            />
+                        ))
+                    }
+                </section>
+            </main>
+        </>
+    );
+}
+
+export default Tasks;
