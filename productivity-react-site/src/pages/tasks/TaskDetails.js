0618916164f87236f12d341599e4bce4c7b63d45
Benjamin Pham <97713069+BenPham14@users.noreply.github.com>
2023-07-28 23:43:05 -0700

    Add styling to new details pane

--- productivity-react-site/src/pages/tasks/TaskDetails.js
+++ productivity-react-site/src/pages/tasks/TaskDetails.js
@@ -3,28 +3,32 @@ import tasks from "./Tasks.module.css";
 
 const TaskDetails = ({selectedTask, editTask}) => {
     const [newTask, setNewTask] = useState("");
-
+    const [button, setButton] = useState(false);
 
     useEffect(() => {
-        setNewTask(selectedTask.label)
+        setNewTask(selectedTask.label);
+        setButton(false)
     }, [selectedTask.label])
 
     const handleSubmit = () => {
         editTask(newTask, selectedTask.id);
+        setButton(false);
     }
 
     return (
         <section className={`${tasks.details} blk-shadow flex column`}>
-            {
-                selectedTask.date &&
+            <div className={`${tasks.header} flex`}>
                 <h5>{selectedTask.date}</h5>
-            }
-            <input 
+                {   
+                    button &&
+                        <button onClick={() => handleSubmit()}>Save</button>
+                }
+            </div>
+            <textarea
                 type="text"
                 value={newTask}
-                onChange={(event) => setNewTask(event.target.value)}
+                onChange={(event) => {setNewTask(event.target.value); setButton(true)}}
             />
-            <button onClick={() => handleSubmit()}>Save</button>
         </section>
     );
 }
