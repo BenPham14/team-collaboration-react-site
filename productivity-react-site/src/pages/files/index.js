183df7d2ca78f1232d2a42710aac51b8665021d2
Benjamin Pham <97713069+BenPham14@users.noreply.github.com>
2023-09-09 17:48:42 -0700

    Finish file upload functionality

--- productivity-react-site/src/pages/files/index.js
+++ productivity-react-site/src/pages/files/index.js
@@ -1,13 +1,46 @@
+import { useContext, useEffect, useState } from "react";
 import Sidebar from "../../components/sidebar";
 import Topbar from "../../components/topbar";
 import files from "./files.module.css";
+import { storage } from "../../config/firebase";
+import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import { AppContext } from "../../context/AppContext";
+import {v4 as uuidv4} from 'uuid';
 
 const Files = () => {
-    const filesList = [
-        {image: "", label: "File Name", createdBy: "Name", createdAt: "1/1/1"},
-        {image: "", label: "File Name", createdBy: "Name", createdAt: "1/1/1"},
-        {image: "", label: "File Name", createdBy: "Name", createdAt: "1/1/1"}
-    ];
+    const [uploadOpen, setUploadOpen] = useState(false);
+    const [fileUpload, setFileUpload] = useState(null);
+    const { currentTeamUID } = useContext(AppContext);
+    const fileListRef = ref(storage, `${currentTeamUID}/`);
+    const [fileList, setFileList] = useState([]);
+
+    useEffect(() => {
+        listAll(fileListRef)
+            .then((response) => {
+                response.items.forEach((item) => {
+                    getDownloadURL(item)
+                        .then((url) => {
+                            setFileList((prev) => [...prev, {downloadURL: url, name: item.name}]);
+                        });
+                        // console.log(response.items[0]);
+                });
+            });
+    }, [currentTeamUID]);
+
+    const uploadFile = () => {
+        if (fileUpload == null) {
+            return;
+        };
+
+        const fileRef = ref(storage, `${currentTeamUID}/${fileUpload.name + uuidv4()}`);
+        uploadBytes(fileRef, fileUpload)
+            .then((snapshot) => {
+                getDownloadURL(snapshot.ref)
+                    .then((url) => {
+                        setFileList((prev) => [...prev, {downloadURL: url, name: snapshot.ref.name}]);
+                    });
+            });
+    };
 
     return (
         <>
@@ -16,25 +49,28 @@ const Files = () => {
                 <Topbar/>
                 <section className={files.list}>
                     <div className={`${files.buttons} flex`}>
-                        <button>+ Upload File</button>
+                        <button onClick={() => setUploadOpen(true)}>+ Upload File</button>
                     </div>
+                    <dialog open={uploadOpen}>
+                        <input type="file" onChange={(event) => setFileUpload(event.target.files[0])}/>
+                        <button onClick={uploadFile}>Upload</button>
+                        <button onClick={() => setUploadOpen(false)}>Cancel</button>
+                    </dialog>
                     <table>
                         <thead>
                             <tr>
-                                <th>Preview</th>
                                 <th>Name</th>
-                                <th>Created By</th>
-                                <th>Created At</th>
+                                {/* <th>Size</th>
+                                <th>Last Modified</th> */}
                             </tr>
                         </thead>
                         <tbody>
                             {
-                                filesList.map((file, index) => (
+                                fileList.map((file, index) => (
                                     <tr key={index} className={files.item}>
-                                        <td><img src={file.image} alt={file.label}/></td>
-                                        <td>{file.label}</td>
-                                        <td>{file.createdBy}</td>
-                                        <td>{file.createdAt}</td>
+                                        <td><a href={file.downloadURL}>{file.name}</a></td>
+                                        {/* <td>{file.size}</td>
+                                        <td>{file.lastModified}</td> */}
                                     </tr>
                                 ))
                             }
